2cb2f44bd70f2a891b33a08666d60afb86670d03
Oleksandra <primakovaaleksandra@gmail.com>
2022-08-10 22:06:18 +0200

    added exsercise1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+//1
+function sumTwoSmallestNumbers(...args) {
+    const sortedArgs = args.sort((a, b) => a - b);
+    return sortedArgs[0] + sortedArgs[1];
+}
+
+console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77));
+console.log(sumTwoSmallestNumbers(1, 3, 2));
+console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
+
